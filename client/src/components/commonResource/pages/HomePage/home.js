1aae948ae6d3d18d98d772d24c0766af1b7fad40
NuhaminMel <132179266+NuhaminMel@users.noreply.github.com>
2024-01-26 23:05:17 -0500

    inital commit

---
+++ client/src/components/commonResource/pages/HomePage/home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Discription from "./discription/discription";
+import Iphone14Pro from "./iphone14Pro/iphone14Pro";
+import IPhone14 from "./iPhone14/iPhone14";
+import IpadPro from "./ipadPro/ipadPro";
+import TradeInAndWwdc from "./tradeInAndWwdc/tradeInAndWwdc";
+import WatchAndMac from "./watchAndMac/watchAndMac";
+import HomePodAndCard from "./homePodAndCard/homePodAndCard";
+// import YouTubeVideos from "./YoutubeVideos/youTubeVideos";
+
+function Home() {
+  return (
+    <div>
+      <Discription />
+      <Iphone14Pro />
+      <IPhone14 />
+      <IpadPro />
+      <TradeInAndWwdc />
+      <WatchAndMac />
+      <HomePodAndCard />
+      {/* <YouTubeVideos /> */}
+    </div>
+  );
+}
+
+export default Home;
